240047648443aa21883cf4e0b8875d21972044e7
Steven Baty <baty_steven@yahoo.com>
2021-01-18 02:35:06 -0700

    add user form to search location based on city,state.. add handler and event listener

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,31 @@
+var userSubmit = document.querySelector("#user-form");
+var locationInputEl = document.querySelector("#location");
 
+var formSubmitHandler = function(event) {
+    //prevent page from refreshing
+    event.preventDefault();
 
-var getWeather = function() {
+    //get value from input element
+    var location = locationInputEl.value.trim();
+
+    getWeather(location);
+};
+
+var getWeather = function(location) {
     //format apiurl w/key
-    var  apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + "London" + "&appid=" + "291b2638328647d2c894b01fa08bcd9e";
+    var  apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + location + "&appid=" + "291b2638328647d2c894b01fa08bcd9e";
 
     fetch(apiUrl).then(function(response) {
         if (response.ok) {
             response.json().then(function(data) {
-                console.log(data);
+                displayWeather(data);
             });
         }
     });
 };
 
-getWeather();
+var displayWeather = function(data) {
+    console.log(data);
+};
+
+userSubmit.addEventListener("submit", formSubmitHandler);
